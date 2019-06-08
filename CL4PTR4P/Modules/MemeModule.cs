c7119e5432246f4a9fa679b420ae9b74f0475485
Eddycted <evandijk@live.nl>
2019-06-09 00:46:44 +0200

    Some cleanup

---
+++ CL4PTR4P/Modules/MemeModule.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace CL4PTR4P.Modules
+{
+    public class MemeModule : ModuleBase<SocketCommandContext>
+    {
+        // TODO: Find and .Mention
+        [Command("thunderfury")]
+        [Summary("Echoes a message.")]
+        public async Task ThunderfuryAsync()
+        {
+            await ReplyAsync($"Misschien wil Demix (Roel) die even vasthouden?");
+        }
+    }
+}
---
+++ CL4PTR4P/Startup.cs
@@ -0,0 +1,146 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CL4PTR4P
+{
+    class Startup
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        private readonly IServiceProvider _services;
+
+        static void Main() => new Startup().MainAsync().GetAwaiter().GetResult();
+
+        private Startup()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug,
+
+                // If you or another service needs to do anything with messages
+                // (eg. checking Reactions, checking the content of edited/deleted messages),
+                // you must set the MessageCacheSize. You may adjust the number as needed.
+                //MessageCacheSize = 50,
+
+                // If your platform doesn't have native WebSockets,
+                // add Discord.Net.Providers.WS4Net from NuGet,
+                // add the `using` at the top, and uncomment this line:
+                //WebSocketProvider = WS4NetProvider.Instance
+            });
+
+            _commands = new CommandService(new CommandServiceConfig
+            {
+                LogLevel = LogSeverity.Debug,
+                CaseSensitiveCommands = false,
+            });
+
+            _client.Log += Log;
+            _commands.Log += Log;
+
+            _services = ConfigureServices();
+
+        }
+        
+        // Setup Dependency Injection
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            return services.BuildServiceProvider();
+        }
+
+        // Example of a logging handler. This can be re-used by addons
+        // that ask for a Func<LogMessage, Task>.
+        private static Task Log(LogMessage message)
+        {
+            switch (message.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity}] {message.Source}: {message.Message} {message.Exception}");
+            Console.ResetColor();
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MainAsync()
+        {
+            // Centralize the logic for commands into a separate method.
+            await InitializeCommands();
+
+            // Login and connect.
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
+
+            // Wait infinitely so your bot actually stays connected.
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private async Task InitializeCommands()
+        {
+            // Either search the program and add all Module classes that can be found.
+            // Module classes MUST be marked 'public' or they will be ignored.
+            // You also need to pass your 'IServiceProvider' instance now,
+            // so make sure that's done before you get here.
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            // Or add Modules manually if you prefer to be a little more explicit:
+            //await _commands.AddModuleAsync<Infomofule>(_services);
+            // Note that the first one is 'Modules' (plural) and the second is 'Module' (singular).
+
+            // Subscribe a handler to see if a message invokes a command.
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            // Bail out if it's a System Message.
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            // We don't want the bot to respond to itself or other bots.
+            if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int pos = 0;
+            // Replace the '!' with whatever character
+            // you want to prefix your commands with.
+            // Uncomment the second half if you also want
+            // commands to be invoked by mentioning the bot instead.
+            if (msg.HasCharPrefix('!', ref pos) /* || msg.HasMentionPrefix(_client.CurrentUser, ref pos) */)
+            {
+                // Create a Command Context.
+                var context = new SocketCommandContext(_client, msg);
+
+                // Execute the command. (result does not indicate a return value, 
+                // rather an object stating if the command executed successfully).
+                var result = await _commands.ExecuteAsync(context, pos, _services);
+
+                // Uncomment the following lines if you want the bot
+                // to send a message if it failed.
+                // This does not catch errors from commands with 'RunMode.Async',
+                // subscribe a handler for '_commands.CommandExecuted' to see those.
+                //if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                //    await msg.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+    }
+}
